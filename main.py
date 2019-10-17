9ad53aea7781b6bd44d1a2b07388ddc380f225cc
Ethras <vladimirmbassi@gmail.com>
2019-10-17 16:24:41 +0200

    playing around with kivy layout

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from kivy.app import App
+from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
 
 
-class MainWindow(Widget):
+class MainWindow(BoxLayout):
+    pass
+
+
+class SideBar(BoxLayout):
+    select_image_button = ObjectProperty(None)
+
+    def select_image(self):
+        print("Select Image")
+
+
+class TopBar(BoxLayout):
+    pass
+
+
+class EditorFrame(BoxLayout):
     pass
 
 
